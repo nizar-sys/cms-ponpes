ef1acf9e44dea152f967742c1d8f8a8f0029a728
WeBowo <setyonobowo24@gmail.com>
2024-07-11 08:08:20 +0700

    Finishing the app

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -6,7 +6,7 @@
 
     <!-- Page Title -->
     <div class="page-title dark-background" data-aos="fade"
-        style="background-image: url({{ asset('/ponpes') }}/assets/img/hero-image1.jpeg)">
+        style="background-image: url({{ asset('/ponpes') }}/assets/img/page-title-bg.webp)">
         <div class="container position-relative">
             <h1>Tentang Kami</h1>
             <nav class="breadcrumbs">
@@ -24,14 +24,16 @@
         <div class="container">
             <div class="row gy-4 justify-content-between align-items-center">
                 <div class="col-lg-6 order-lg-2 position-relative" data-aos="zoom-out">
-                    <img src="{{ asset($about->image) }}" alt="Image" class="img-fluid" />
+                    <img src="{{ $about && $about->image ? asset($about->image) : asset('assets/img/example-image.jpg') }}"
+                        alt="Image" class="img-fluid" />
                 </div>
                 <div class="col-lg-5 order-lg-1" data-aos="fade-up" data-aos-delay="100">
-                    <h2 class="content-title mb-4">{{ $about->title }}</h2>
+                    <h2 class="content-title mb-4">{{ $about->title ?? 'Default Title' }}</h2>
                     <p class="mb-4">
-                        {!! $about->description !!}
+                        {!! $about->description ?? 'Default Description' !!}
                     </p>
-                    <p><a href="{{ asset($about->resume) }}" download target="_blank" class="btn-cta">Download Brosur</a>
+                    <p class="mt-5"><a href="{{ asset($about->resume ?? 'assets/img/example-image.jpg') }}" download
+                            target="_blank" class="btn-cta">Download Brosur</a>
                     </p>
                 </div>
             </div>
